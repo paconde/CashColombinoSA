eab49ab35b0638b46807baf7c8bad6eba079992a
paconde <fcocondeh@hotmail.com>
2017-05-23 19:40:10 +0200

    ver 1.1 Creando botones botones centrales

--- app/src/main/java/com/fconde/cashcolombinoapp/frg_bottom_bar.java
+++ app/src/main/java/com/fconde/cashcolombinoapp/frg_bottom_bar.java
@@ -2,11 +2,13 @@ package com.fconde.cashcolombinoapp;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 /**
  * Created by FConde on 22/05/2017.
@@ -14,7 +16,6 @@ import android.widget.ImageButton;
 
 public class frg_bottom_bar extends Fragment {
 
-    ImageButton imgBtnContacto, imgBtnFacebook, imgBtnGoogle, imgBtnBlog, imgBtnCompartir;
 
     public frg_bottom_bar(){
 
@@ -25,8 +26,51 @@ public class frg_bottom_bar extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
-        View view;
+        ImageButton imgBtnContacto, imgBtnFacebook, imgBtnGoogle, imgBtnBlog, imgBtnCompartir;
+
+        final View view;
         view = inflater.inflate(R.layout.lay_frg_bottom_bar, container);
+
+        imgBtnContacto = (ImageButton)view.findViewById(R.id.imgBttContacto);
+        imgBtnFacebook = (ImageButton)view.findViewById(R.id.imgBttFacebook);
+        imgBtnGoogle = (ImageButton)view.findViewById(R.id.imgBttGooglePlus);
+        imgBtnBlog = (ImageButton)view.findViewById(R.id.imgBttBlog);
+        imgBtnCompartir = (ImageButton)view.findViewById(R.id.imgBttCompartir);
+
+        imgBtnContacto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(view.getContext(),"Contacto", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        imgBtnFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(view.getContext(),"Facebook", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        imgBtnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(view.getContext(),"Google plus", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        imgBtnBlog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(view.getContext(),"Blog", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        imgBtnCompartir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(view.getContext(),"Compartir",Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return view;
 
