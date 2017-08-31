4d07abfad836f30c37f08bf9d667370102bbf0d1
paconde <fcocondeh@hotmail.com>
2017-08-31 19:15:14 +0200

    ver 1.1 Creando la activity pedidos

---
+++ app/src/main/java/com/fconde/cashcolombinoapp/activities/ContactoActivity.java
@@ -0,0 +1,15 @@
+package com.fconde.cashcolombinoapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fconde.cashcolombinoapp.R;
+
+public class ContactoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacto);
+    }
+}
